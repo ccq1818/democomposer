d30cae5ea78e10c36acfed7f324fffad7cefb130
unknown <348578420@qq.com>
2016-07-24 01:18:34 +0800

    测试项目

---
+++ src/Democomposer/Demoobj.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asuspc
+ * Date: 2016/7/24
+ * Time: 0:43
+ */
+
+namespace Democomposer;
+
+
+class Demoobj
+{
+    public function show(){
+        return 'this is Democomposer';
+    }
+}
